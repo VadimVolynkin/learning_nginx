2f636302ef2ba23a166002a7cbf7ac2c131dfa3d
Vadim Volynkin <71ooo@mail.ru>
2021-07-09 13:43:39 +0300

    Init

---
+++ layer7.py
@@ -0,0 +1,39 @@
+# ===== Layer 7 Application
+GET / 10.0.0.3 80
+http headers cookies content-type etc.
+
+# ===== Layer 6 Presentation
+Encrypt
+
+# ===== Layer 5 Session
+
+
+# ===== Layer 4 Transport
+TCP UDP ports 
+
+# ===== Layer 3 Network
+Ip adress 10.0.0.5
+
+# ===== Layer 2 Data link
+Mac adress
+
+# ===== Layer 1 Physical
+Байткод - нули и единицы
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
